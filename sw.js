d69fba95b42a9a2414db2c00f6b936f5b3439424
svsPortal <kakkijo3faz@gmail.com>
2026-09-21 09:05:20 +0900

    Build VR platform beta UI v1

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="vr-beta-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","styles.css","app.js","manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
